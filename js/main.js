1663d06f0ee9bb905512b42afe11089cab02e9d6
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-11-07 15:39:36 +0100

    grid members + cards

--- js/main.js
+++ js/main.js
@@ -2,31 +2,37 @@ const members=[
     {
         'NameMember' : "Wayne Barnett",
         'role' : 'Founder & CEO',
-        'imgSource': 'wayne-barnett-founder-ceo.jpg'
+        'imgSource': 'img/wayne-barnett-founder-ceo.jpg'
 
     },
     {
         'NameMember' : "Walter Gordon",
         'role' : 'Office Manager',
-        'imgSource': 'walter-gordon-office-manager.jpg'
+        'imgSource': 'img/walter-gordon-office-manager.jpg'
+
+    },
+    {
+        'NameMember' : "Angela Carrol",
+        'role' : 'Chief Editor',
+        'imgSource': 'img/angela-caroll-chief-editor.jpg'
 
     },
     {
         'NameMember' : "Angela Lopez",
         'role' : 'Social Media Manager',
-        'imgSource': 'angela-lopez-social-media-manager.jpg'
+        'imgSource': 'img/angela-lopez-social-media-manager.jpg'
 
     },
     {
         'NameMember' : "Scott Estrada",
         'role' : 'Developer',
-        'imgSource': 'scott-estrada-developer.jpg'
+        'imgSource': 'img/scott-estrada-developer.jpg'
 
     },
     {
         'NameMember' : "Barbara Ramos",
         'role' : 'Graphic Designer',
-        'imgSource': 'barbara-ramos-graphic-designer.jpg'
+        'imgSource': 'img/barbara-ramos-graphic-designer.jpg'
 
     },
 ];
@@ -35,15 +41,18 @@ for (let index = 0; index < members.length; index++) {
     const element = members[index];
     console.log(element.role);
 }
-
-let el=document.getElementById('members');
+let el=document.getElementById('card-grid');
 for (let index = 0; index < members.length; index++) {
     const element = members[index];
     el.innerHTML+=`
-            <tr>
-                <td>${element.NameMember}</td>
-                <td>${element.role}</td>
-                <td>${element.imgSource}</td>
-            </tr>
+    <div class="card">
+        <div class="card-img">
+            <img src="${element.imgSource}" alt="">
+        </div>
+        <div class="who">
+            <h3>${element.NameMember}</h3>
+            <div>${element.role}</div>
+        </div>
+    </div>
     `
 }
