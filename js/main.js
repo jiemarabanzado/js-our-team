bbb64c51999c31331d7ee1940c93df0ad707da63
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-11-07 14:59:29 +0100

    creazione cartella

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const members=[
+    {
+        'NameMember' : "Wayne Barnett",
+        'role' : 'Founder & CEO',
+        'imgSource': 'wayne-barnett-founder-ceo.jpg'
+
+    },
+    {
+        'NameMember' : "Walter Gordon",
+        'role' : 'Office Manager',
+        'imgSource': 'walter-gordon-office-manager.jpg'
+
+    },
+    {
+        'NameMember' : "Angela Lopez",
+        'role' : 'Social Media Manager',
+        'imgSource': 'angela-lopez-social-media-manager.jpg'
+
+    },
+    {
+        'NameMember' : "Scott Estrada",
+        'role' : 'Developer',
+        'imgSource': 'scott-estrada-developer.jpg'
+
+    },
+    {
+        'NameMember' : "Barbara Ramos",
+        'role' : 'Graphic Designer',
+        'imgSource': 'barbara-ramos-graphic-designer.jpg'
+
+    },
+];
+console.log(members)
+for (let index = 0; index < members.length; index++) {
+    const element = members[index];
+    console.log(element.role);
+    
+}
